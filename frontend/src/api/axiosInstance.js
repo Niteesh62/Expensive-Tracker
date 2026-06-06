13c47d36c73cbf296d45e1645b1e9776df52a31b
Niteesh <niteeshravi343@gmail.com>
2026-06-07 00:39:37 +0530

    Added log to check be url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
     import.meta.env.VITE_API_URL ||
     "http://localhost:8000/api";
-
+console.log("API Base URL:", API_BASE_URL);
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
     timeout: 10000,
